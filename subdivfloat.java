d7affabb5658207b68ca135cb832b1442bbd1390
Akshu2325 <43032649+Akshu2325@users.noreply.github.com>
2018-09-07 12:31:19 +0530

    Add files via upload

---
+++ subdivfloat.java
@@ -0,0 +1,11 @@
+class Main {
+  public static void main(String[] args) {
+
+   float a = 10.5f,b = 5.3f,sub;
+   sub = a-b;
+   float c = 2.8f, d = 4.2f, div;
+   div = c*d;
+
+   System.out.println(sub+" "+div);
+  }
+}
