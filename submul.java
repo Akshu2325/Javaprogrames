a2753f2d6fc47831b38986e8eaf2b67d9aeb8865
Akshu2325 <43032649+Akshu2325@users.noreply.github.com>
2018-09-07 12:23:54 +0530

    Add files via upload

---
+++ submul.java
@@ -0,0 +1,11 @@
+class Main {
+  public static void main(String[] args) {
+
+   int a = 450,b = 100,sub;
+   sub = a-b;
+   int c = 30, d = 3, mul;
+   mul = c*d;
+
+   System.out.println(sub+" "+mul);
+  }
+}
