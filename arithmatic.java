e4c11719f62675507db1ef75b860d610621428fb
Akshu2325 <43032649+Akshu2325@users.noreply.github.com>
2018-09-07 12:10:24 +0530

    Add files via upload

---
+++ arithmatic.java
@@ -0,0 +1,11 @@
+class Main {
+  public static void main(String[] args) {
+
+   int num1 = 450,num2 = 100,sum;
+   sum = num1 + num2;
+   int num3 = 30, num4 = 3, div;
+   div = num3/num4;
+
+   System.out.println(sum+" "+div);
+  }
+}
