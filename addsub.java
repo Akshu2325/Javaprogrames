546d3cda465ac1406a2540d6e0219785378ff35d
Akshu2325 <43032649+Akshu2325@users.noreply.github.com>
2018-09-07 14:54:14 +0530

    Add files via upload

---
+++ addsub.java
@@ -0,0 +1,13 @@
+class Main {
+  public static void main(String[] args) {
+
+int a=57;
+double e,g;
+float b=20.3f,c=13.7f;
+e=a+b+c;
+g=(b-c)+a;
+System.out.println(e);
+System.out.println(g);
+
+  }
+}
