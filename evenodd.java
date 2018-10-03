cb47db2cb91fc9536aaa48e919a1fd9b01a30390
Akshu2325 <43032649+Akshu2325@users.noreply.github.com>
2018-10-03 12:40:44 +0530

    Create evenodd.java

---
+++ evenodd.java
@@ -0,0 +1,13 @@
+public class numbers{
+
+     public static void main(String []args){
+         int n=7;
+    System.out.println("Enter an Integer number:");
+
+     if ( n ==7)
+        System.out.println("even");
+     else
+        System.out.println("odd");
+       
+     }
+}
