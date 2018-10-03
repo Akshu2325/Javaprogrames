c905d7e16eb1292a88387b887bb1520f3a3251a6
Akshu2325 <43032649+Akshu2325@users.noreply.github.com>
2018-10-03 12:30:56 +0530

    Update positive.java

--- positive.java
+++ positive.java
@@ -1,18 +1,18 @@
 public class numbers{
 
      public static void main(String []args){
-         int number=5;
-        if(number > 0)
+         int n=5;
+        if(n > 0)
         {
             System.out.println(" positive");
         }
-        else if(number < 0)
+        else if(n < 0)
         {
             System.out.println("negative");
         }
         else
         {
-            System.out.println(number+" is neither positive nor negative");
+            System.out.println(n+" is neither positive nor negative");
         }
        
      }
