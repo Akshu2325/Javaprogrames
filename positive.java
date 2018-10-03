790722ca2ff9d9e768ca4f1b8686d7fdd4c6a38c
Akshu2325 <43032649+Akshu2325@users.noreply.github.com>
2018-10-03 12:21:52 +0530

    Create positive.java

---
+++ positive.java
@@ -0,0 +1,19 @@
+public class numbers{
+
+     public static void main(String []args){
+         int number=5;
+        if(number > 0)
+        {
+            System.out.println(" positive");
+        }
+        else if(number < 0)
+        {
+            System.out.println("negative");
+        }
+        else
+        {
+            System.out.println(number+" is neither positive nor negative");
+        }
+       
+     }
+}
