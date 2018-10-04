d5a1c113f60787f828eaa1c7288cce05b50abfe4
Akshu2325 <43032649+Akshu2325@users.noreply.github.com>
2018-10-04 11:31:17 +0530

    Update evenodd.java

--- evenodd.java
+++ evenodd.java
@@ -1,13 +1,13 @@
 public class numbers{
 
      public static void main(String []args){
-         int n=7;
+         int n=5;
     System.out.println("Enter an Integer number:");
 
-     if ( n ==7)
-        System.out.println("even");
-     else
+     if ( n ==5)
         System.out.println("odd");
+     else
+        System.out.println("even");
        
      }
 }
