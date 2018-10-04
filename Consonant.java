d14f9e5ed3e91460a9a53733133ed9d98f27d1ab
Akshu2325 <43032649+Akshu2325@users.noreply.github.com>
2018-10-04 12:05:52 +0530

    Create Consonant.java

---
+++ Consonant.java
@@ -0,0 +1,13 @@
+public class VowelConsonant {
+
+    public static void main(String[] args) {
+
+        char x = 'f';
+
+        if(x == 'a' || x == 'e' || x == 'i' || x == 'o' || x == 'u'|| x == 'f' )
+            System.out.println(x + " is Consonant");
+        else
+            System.out.println(x + " is vowel");
+
+    }
+}
