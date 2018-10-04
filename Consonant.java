c90666bc4a5d7fe60471925c8df34d59714e8cb8
Akshu2325 <43032649+Akshu2325@users.noreply.github.com>
2018-10-04 12:08:44 +0530

    Update Consonant.java

--- Consonant.java
+++ Consonant.java
@@ -2,9 +2,9 @@ public class VowelConsonant {
 
     public static void main(String[] args) {
 
-        char x = 'f';
+        char x = 'z';
 
-        if(x == 'a' || x == 'e' || x == 'i' || x == 'o' || x == 'u'|| x == 'f' )
+        if(x == 'a' || x == 'e' || x == 'i' || x == 'o' || x == 'u'|| x == 'z' )
             System.out.println(x + " is Consonant");
         else
             System.out.println(x + " is vowel");
