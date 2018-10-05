40f150565ccf96d2aa9e0120a5c0d1d9cd7b825d
Akshu2325 <43032649+Akshu2325@users.noreply.github.com>
2018-10-05 12:16:56 +0530

    Create largestnum.java

---
+++ largestnum.java
@@ -0,0 +1,15 @@
+public class largestnum{
+
+     public static void main(String []args){
+     int a = 1, b = 2, c = 3;
+
+      if( a >= b && a >= c)
+          System.out.println(a);
+
+      else if (b >= a && b >= c)
+          System.out.println(b);
+
+      else
+          System.out.println(c);
+     }
+}
