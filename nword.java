88ae1acf285504fd1edba1ad8a6fc9dd19fae493
Akshu2325 <43032649+Akshu2325@users.noreply.github.com>
2018-10-05 12:36:47 +0530

    Create nword.java

---
+++ nword.java
@@ -0,0 +1,11 @@
+public class nword{
+
+     public static void main(String []args){
+   int n=2;
+String myStr = "Hello";
+		
+for(int counter=0; counter < n; counter++) 
+	System.out.println(myStr);
+      
+     }
+}
