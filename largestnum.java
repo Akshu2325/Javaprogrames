3d5fe419d68bf91c1518283e4a1eefde8d534930
Akshu2325 <43032649+Akshu2325@users.noreply.github.com>
2018-10-05 12:18:25 +0530

    Update largestnum.java

--- largestnum.java
+++ largestnum.java
@@ -1,7 +1,7 @@
 public class largestnum{
 
      public static void main(String []args){
-     int a = 1, b = 2, c = 3;
+     int a = 3, b = 4, c = 5;
 
       if( a >= b && a >= c)
           System.out.println(a);
